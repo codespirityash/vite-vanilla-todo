3b444a4aca19109231ef32b54b43693d09454ab1
codespirityash <codespirityash@gmail.com>
2024-11-06 23:18:26 +0530

    add functionality to button

--- src/main.js
+++ src/main.js
@@ -1,1 +1,8 @@
-const button = document.getElementById("button")
+
+const button = document.getElementById("button");
+
+button.addEventListener("click", () => {
+  const text = document.createElement("div");
+  text.innerText = "HELLO";
+  document.body.append(text);
+});
