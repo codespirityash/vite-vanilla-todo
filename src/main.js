7c151b59e0843b0e2a731b3c9787813dc91fcc2b
yashweblife <yashweblife20@gmail.com>
2024-11-18 11:51:37 -0600

    reset

--- src/main.js
+++
@@ -1,8 +0,0 @@
-
-const button = document.getElementById("button");
-
-button.addEventListener("click", () => {
-  const text = document.createElement("div");
-  text.innerText = "HELLO";
-  document.body.append(text);
-});
